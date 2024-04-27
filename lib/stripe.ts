34442e38ab0632e03dcf5de3aae2344ba11a7881
cyril poornaraj <cyrilpoornaraj@gmail.com>
2024-04-27 10:18:42 +0530

    final

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,3 +4,8 @@ export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2023-10-16",
   typescript: true,
 });
+
+// var instance = new Razorpay({
+//     key_id: 'YOUR_KEY_ID',
+//     key_secret: 'YOUR_KEY_SECRET',
+//   });
